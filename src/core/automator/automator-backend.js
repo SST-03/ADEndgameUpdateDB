cfab0adfae59c6e954f48d7172063d7f82299acb
Supersonic Seven <b.rockholz@icloud.com>
2025-10-19 21:27:55 -0400

    Update automator-backend.js
    Mastery 21

--- src/core/automator/automator-backend.js
+++ src/core/automator/automator-backend.js
@@ -451,6 +451,7 @@ export const AutomatorBackend = {
   },
 
   get currentInterval() {
+    if (EndgameMastery(21).isBought) return Math.clampMin(Math.pow(0.94, Currency.realities.value) * 500, 1);
     return Math.clampMin(Math.pow(0.994, Currency.realities.value) * 500, 1);
   },
 
