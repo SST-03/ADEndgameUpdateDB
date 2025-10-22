3f034054007a6c49fee1d68d13dabbd0cffb2993
Supersonic Seven <b.rockholz@icloud.com>
2025-10-21 21:06:08 -0400

    Update endgame-masteries.js
    The add function just doesn’t work whatsoever lol

--- src/core/endgame-masteries/endgame-masteries.js
+++ src/core/endgame-masteries/endgame-masteries.js
@@ -87,6 +87,7 @@ export function respecEndgameMasteries() {
   GameCache.endgameMasteries.invalidate();
   Tab.endgame.masteries.show();
   GameCache.currentMasteryTree.invalidate();
+  Currency.endgameMasteries.maxSkills = Currency.endgameMasteries.skills.plus(EndgameSkills.calculateEndgameMasteriesCost());
 }
 
 export class EndgameMasteriesState extends GameMechanicState {
