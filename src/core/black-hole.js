f4c3527dc11e393b5dacf70649d76cc05f27f0f4
Supersonic Seven <b.rockholz@icloud.com>
2025-11-03 10:49:15 -0500

    Update black-hole.js
    I’m not happy Bob. NOT HAPPY.

--- src/core/black-hole.js
+++ src/core/black-hole.js
@@ -507,10 +507,10 @@ export const BlackHoles = {
     // We could do this.autoPauseData(realTime)[1] here but that seems less clear.
     // Using _ as an unused variable should be reasonable.
     // eslint-disable-next-line no-unused-vars
-    const [_, realerTime] = new Decimal(this.autoPauseData(realTime));
-    const effectivePeriods = this.realTimePeriodsWithBlackHoleEffective(realerTime, speedups);
+    const [_, realerTime] = this.autoPauseData(realTime);
+    const effectivePeriods = this.realTimePeriodsWithBlackHoleEffective(new Decimal(realerTime), speedups);
     // This adds in time with black holes paused at the end of the list.
-    effectivePeriods[0] = effectivePeriods[0].add(realTime.sub(realerTime));
+    effectivePeriods[0] = effectivePeriods[0].add(realTime.sub(new Decimal(realerTime)));
     return effectivePeriods
       .map((period, i) => speedups[i].times(period))
       .sum();
