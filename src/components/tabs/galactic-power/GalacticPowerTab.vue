da8feb0cf05f89258d55be90a86a5f2f84f669a4
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 18:06:20 -0500

    Update GalacticPowerTab.vue
    Implementing Tab Overhaul

--- src/components/tabs/galactic-power/GalacticPowerTab.vue
+++ src/components/tabs/galactic-power/GalacticPowerTab.vue
@@ -8,12 +8,16 @@ export default {
   },
   data() {
     return {
+      galacticPower: new Decimal(),
       nextPow: 0
     };
   },
   computed: {
     powers() {
       return GalacticPowers.all;
+    },
+    rows() {
+      return Math.ceil(this.milestones.length);
     },
     nextAtDisplay() {
       const first = this.nextPow?.id === 1;
@@ -27,16 +31,30 @@ export default {
   },
   methods: {
     update() {
+      this.galacticPower.copyFrom(Currency.galacticPower.value.floor());
       this.nextPow = GalacticPower.nextPow;
+    },
+    getPower(row, column) {
+      return () => this.powers[(row - 1) + column - 1];
     }
   }
 };
 </script>
 
 <template>
-  <div>
-    <div>
-      <GalacticPowerRow/>
+  <div class="l-eternity-milestone-grid">
+    <div>You have {{ quantify("Galactic Power", galacticPower, 3) }}.</div>
+    <div
+      v-for="row in rows"
+      :key="row"
+      class="l-eternity-milestone-grid__row"
+    >
+      <GalacticPowerRow
+        v-for="column in 1"
+        :key="row + column"
+        :get-milestone="getPower(row, column)"
+        class="l-eternity-milestone-grid__cell"
+      />
     </div>
     <div>{{ nextAtDisplay }}</div>
   </div>
