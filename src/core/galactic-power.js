43d3837b1dbb2eaf8695245d6d76b72d74a7f141
Supersonic Seven <b.rockholz@icloud.com>
2025-11-25 14:11:11 -0500

    Update galactic-power.js
    I’m so fucking tired

--- src/core/galactic-power.js
+++ src/core/galactic-power.js
@@ -42,8 +42,15 @@ export const GalacticPower = {
 export function getGalacticPowerGainPerSecond() {
   const allGalaxies = Replicanti.galaxies.total + player.galaxies + player.dilation.totalTachyonGalaxies;
   const galaxyFactor = Math.max(allGalaxies / 100000, 1);
-  const celMatterFactor = Math.max(Math.pow(Decimal.log10(player.endgame.celestialMatter.add(1)) / 10, 2), 1);
-  const imaginaryFactor = Math.max(Math.pow(Decimal.log10(player.reality.imaginaryMachines + 1), 1.5), 1);
-  const galaxyExponent = Math.min(1 + Math.pow(allGalaxies / 2000000, 2), 100);
-  return Pelle.isDoomed ? new Decimal(0) : Decimal.pow(galaxyFactor * celMatterFactor * imaginaryFactor / 1000, galaxyExponent);
+  const celMatterFactor = Math.max(Math.pow(Decimal.log10(player.endgame.celestialMatter.add(1)) / 10, 4), 1);
+  const imaginaryFactor = Math.max(Math.pow(Decimal.log10(player.reality.imaginaryMachines + 1), 2.5), 1);
+  const staticDivisor = 1e7;
+  const base = galaxyFactor * celMatterFactor * imaginaryFactor / staticDivisor;
+  const galaxyExponent1 = Math.max(Math.min(Math.pow(allGalaxies / 1680000, 6.4), 4), 1);
+  const galaxyExponent2 = Math.max(Math.min(Math.pow(allGalaxies / 1960000, 15), 5), 1);
+  const galaxyExponent3 = Math.max(Math.min(Math.pow(allGalaxies / 2160000, 5), 1.6), 1);
+  const galaxyExponent4 = Math.max(Math.min(Math.pow(allGalaxies / 4500000, 0.5), 1.25), 1);
+  const galaxyExponent5 = Math.max(Math.min(Math.pow(allGalaxies / 8000000, 0.25), 2.5), 1);
+  const exponent = galaxyExponent1 * galaxyExponent2 * galaxyExponent3 * galaxyExponent4 * galaxyExponent5;
+  return Pelle.isDoomed ? new Decimal(0) : Decimal.pow(base, exponent);
 }
