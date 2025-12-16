b505cc192190c36abd9fe87371093084b1b97259
Supersonic Seven <b.rockholz@icloud.com>
2025-12-16 13:49:31 -0500

    Update ra.js
    Effarig 80

--- src/core/celestials/ra/ra.js
+++ src/core/celestials/ra/ra.js
@@ -386,7 +386,9 @@ export const Ra = {
     this.updateAlchemyFlow(realityRealTime);
   },
   get alchemyResourceCap() {
-    return ExpansionPack.effarigPack.isBought ? Math.max(25000, player.records.bestEndgame.glyphLevel / 3) : 25000;
+    return ExpansionPack.effarigPack.isBought
+      ? Math.max(25000, (player.records.bestEndgame.glyphLevel / 3) * Ra.unlocks.alchemyCapIncrease.effectOrDefault(1))
+      : 25000 * Ra.unlocks.alchemyCapIncrease.effectOrDefault(1);
   },
   get momentumValue() {
     const hoursFromUnlock = TimeSpan.fromMilliseconds(new Decimal(player.celestials.ra.momentumTime)).totalHours.toNumber();
