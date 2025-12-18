bfcbba818120660c4da6ade892d1b3827862b4f7
Supersonic Seven <b.rockholz@icloud.com>
2025-12-18 16:45:45 -0500

    Update TickspeedRow.vue
    Obv purchase gonna overflow

--- src/components/tabs/antimatter-dimensions/TickspeedRow.vue
+++ src/components/tabs/antimatter-dimensions/TickspeedRow.vue
@@ -40,7 +40,7 @@ export default {
     },
     upgradeCount() {
       const purchased = this.purchasedTickspeed;
-      if (!this.freeTickspeed) return quantifyInt("Purchased Upgrade", purchased);
+      if (!this.freeTickspeed) return quantifyHybridLarge("Purchased Upgrade", purchased);
       if (purchased === 0 || this.isContinuumActive) return `${formatHybridLarge(this.freeTickspeed, 3)} Free Upgrades`;
       return `${formatHybridLarge(purchased, 3)} Purchased + ${formatHybridLarge(this.freeTickspeed, 3)} Free`;
     }
