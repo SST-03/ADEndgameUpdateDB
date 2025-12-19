2c93639d3d1a0c67827e9a5f221dabd6859d933a
Supersonic Seven <b.rockholz@icloud.com>
2025-12-19 13:04:00 -0500

    Create bulk-singularity-autobuyer.js
    Starting the stream

---
+++ src/core/autobuyers/bulk-singularity-autobuyer.js
@@ -0,0 +1,45 @@
+import { AutobuyerState } from "./autobuyer";
+
+export class BulkSingularityAutobuyerState extends AutobuyerState {
+  get data() {
+    return player.auto.bulkSingularity;
+  }
+
+  get name() {
+    return `Bulk Singularity`;
+  }
+
+  get isUnlocked() {
+    return ExpansionPack.laitelaPack.isBought;
+  }
+
+  get lowerBound() {
+    return this.data.lowerBound;
+  }
+
+  set lowerBound(value) {
+    this.data.lowerBound = value;
+  }
+
+  get upperBound() {
+    return this.data.upperBound;
+  }
+
+  set upperBound(value) {
+    this.data.upperBound = value;
+  }
+
+  get bulk() {
+    return 0;
+  }
+
+  tick() {
+    if (Singularity.timePerCondense.gt(this.upperBound)) {
+      Singularity.decreaseCap();
+    }
+
+    if (Singularity.timePerCondense.lt(this.lowerBound)) {
+      Singularity.increaseCap();
+    }
+  }
+}
