0aa5ad25fe806348be4385babb6f18a7f9c02065
Supersonic Seven <b.rockholz@icloud.com>
2025-12-20 15:04:57 -0500

    Update dark-matter-dimension.js
    Fixing softcaps and making them harsher

--- src/core/celestials/laitela/dark-matter-dimension.js
+++ src/core/celestials/laitela/dark-matter-dimension.js
@@ -278,16 +278,16 @@ export const DarkMatterDimensions = {
         const ticks = Decimal.floor(new Decimal(dim.timeSinceLastUpdate).div(dim.interval));
         const productionDM = dim.amount.times(ticks).times(dim.powerDM);
         if (tier === 1) {
-          const nerfedProduction = Decimal.min(productionDM, DC.E10000).times(
-            Decimal.max(Decimal.pow(productionDM.div(DC.E10000), 1), 0.5));
-          const nerfedProductionFull = Decimal.min(nerfedProduction, DC.E100000).times(
-            Decimal.max(Decimal.pow(nerfedProduction.div(DC.E100000), 1), 0.1));
+          const nerfedProduction = Decimal.min(productionDM, DC.E1000).times(
+            Decimal.pow(Decimal.max(productionDM.div(DC.E1000), 1), 0.5));
+          const nerfedProductionFull = Decimal.min(nerfedProduction, DC.E10000).times(
+            Decimal.pow(Decimal.max(nerfedProduction.div(DC.E10000), 1), 0.1));
           Currency.darkMatter.add(Decimal.min(nerfedProductionFull, Laitela.darkMatterCap.sub(Currency.darkMatter.value)));
         } else {
-          const nerfedProduction = Decimal.min(productionDM, DC.E10000).times(
-            Decimal.max(Decimal.pow(productionDM.div(DC.E10000), 1), 0.5));
-          const nerfedProductionFull = Decimal.min(nerfedProduction, DC.E100000).times(
-            Decimal.max(Decimal.pow(nerfedProduction.div(DC.E100000), 1), 0.1));
+          const nerfedProduction = Decimal.min(productionDM, DC.E1000).times(
+            Decimal.pow(Decimal.max(productionDM.div(DC.E1000), 1), 0.5));
+          const nerfedProductionFull = Decimal.min(nerfedProduction, DC.E10000).times(
+            Decimal.pow(Decimal.max(nerfedProduction.div(DC.E10000), 1), 0.1));
           DarkMatterDimension(tier - 1).amount = DarkMatterDimension(tier - 1).amount.plus(nerfedProductionFull);
         }
         Currency.darkEnergy.add(ticks.mul(dim.powerDE));
