8fab943d3dd2766a984f8f3062799067e2afd6fc
Supersonic Seven <b.rockholz@icloud.com>
2025-12-29 11:31:23 -0500

    Create galaxy-generator-autobuyer.js
    Saving...

---
+++ src/core/autobuyers/galaxy-generator-autobuyer.js
@@ -0,0 +1,40 @@
+import { IntervaledAutobuyerState } from "./autobuyer";
+
+export class GalaxyGeneratorAutobuyerState extends IntervaledAutobuyerState {
+  get _upgradeName() { return ["additive", "multiplicative", "antimatterMult", "IPMult", "EPMult", "RSMult"][this.id - 1]; }
+
+  get data() {
+    return player.auto.galaxyGenerator.all[this.id - 1];
+  }
+
+  get name() {
+    return ["Base Galaxy Multiplier", "Multiplicative Galaxy Multiplier", "Antimatter Multiplier", "Infinity Point Multiplier", "Eternity Point Multiplier", "Reality Shard Multiplier"][this.id - 1];
+  }
+
+  get interval() {
+    return 1000;
+  }
+
+  get isUnlocked() {
+    return ExpansionPack.pellePack.isBought;
+  }
+
+  get resetTickOn() {
+    return PRESTIGE_EVENT.ENDGAME;
+  }
+
+  get bulk() {
+    return 1;
+  }
+
+  tick() {
+    super.tick();
+    const upgradeName = this._upgradeName;
+    GalaxyGeneratorUpgrades[upgradeName].purchase(this.bulk);
+  }
+
+  static get entryCount() { return 6; }
+  static get autobuyerGroupName() { return "Galaxy Generator Upgrade"; }
+  static get isActive() { return player.auto.galaxyGenerator.isActive; }
+  static set isActive(value) { player.auto.galaxyGenerator.isActive = value; }
+}
