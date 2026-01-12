401151d11d10edc2943fbe62769b3e7e054c3c91
Supersonic Seven <b.rockholz@icloud.com>
2026-01-12 13:40:24 -0500

    Update ReplicantiGainText.vue
    Literally I can adjust this later if needbe

--- src/components/tabs/replicanti/ReplicantiGainText.vue
+++ src/components/tabs/replicanti/ReplicantiGainText.vue
@@ -53,7 +53,7 @@ export default {
       }
 
       const galaxiesPerSecond = log10GainFactorPerTickUncapped.times(ticksPerSecond / LOG10_MAX_VALUE);
-      const timeFromZeroRG = galaxies => 50 * Decimal.ln((galaxies.add(49.5)).div(49.5));
+      const timeFromZeroRG = galaxies => Decimal.ln((galaxies.add(49.5)).div(49.5)).times(50).toNumber();
       let baseGalaxiesPerSecond, effectiveMaxRG, effectiveCurrentRG;
       if (RealityUpgrade(6).isBought && !Pelle.isDoomed) {
         baseGalaxiesPerSecond = galaxiesPerSecond.divide(RealityUpgrade(6).effectValue);
