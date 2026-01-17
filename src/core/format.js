fb7c129820f9a3ec6c0902d1dcdc9527a041bb0b
Supersonic Seven <b.rockholz@icloud.com>
2026-01-17 09:59:57 -0500

    Update format.js
    Rounding accuracy

--- src/core/format.js
+++ src/core/format.js
@@ -10,7 +10,7 @@ window.format = function format(value, places = 0, placesUnder1000 = 0) {
   if (isEND()) return "END";
   if (!(value instanceof Decimal)) value = new Decimal(value);
   if (value.lt("e9e15")) return Notations.current.format(value, places, placesUnder1000, 3);
-  return LNotations.current.formatLDecimal(value, places);
+  return LNotations.current.formatLDecimal(value, 4);
 };
 
 window.formatInt = function formatInt(value) {
