c63b64034421c21718d08e3e5b6a14fb381556b7
Supersonic Seven <b.rockholz@icloud.com>
2026-01-19 16:42:10 -0500

    Update EtherealTab.vue
    Finishing the Ethereal (for now)

--- src/components/tabs/ethereal/EtherealTab.vue
+++ src/components/tabs/ethereal/EtherealTab.vue
@@ -1,33 +1,63 @@
 <script>
 export default {
-  name: "EtherealTab"
+  name: "EtherealTab",
+  data() {
+    return {
+      etherealPower: new Decimal(),
+      etherealPowerPerSecond: new Decimal(),
+      cosmicSector: 0,
+      nextSectorAt: new Decimal(),
+      sectorBoost: new Decimal()
+    };
+  },
+  methods: {
+    update() {
+      this.etherealPower.copyFrom(Currency.etherealPower.value);
+      this.etherealPowerPerSecond.copyFrom(getEtherealPowerGainPerSecond());
+      this.cosmicSector = Ethereal.cosmicSector;
+      this.nextSectorAt.copyFrom(Ethereal.sectorThreshold);
+      this.sectorBoost.copyFrom(Ethereal.sectorBoost);
+    }
+  }
 };
 </script>
 
 <template>
   <div>
     <div>
-      <br>
-      <div class="nyi-ethereal-text">
-      <b>
-        Below is the test style of numbers in the Ethereal
-      </b>
-      </div>
-      <br>
-      <div class="really-cool-ethereal-text">
-        9007199254740992
-      </div>
+      <span class="c-normal-ethereal-text">You have </span>
+      <span class="c-really-cool-ethereal-text">{{ format(etherealPower, 2, 2) }}</span>
+      <span class="c-normal-ethereal-text"> Ethereal Power. </span>
+      <span class="c-really-cool-ethereal-text">+{{ format(etherealPowerPerSecond, 2, 2) }}/s</span>
+    </div>
+    <div>
+      <span class="c-normal-ethereal-text">
+        Ethereal Power income is based on Celestial Points, Singularities, and Reality Machine amounts.
+      </span>
+      <span v-if="false">It is also based on Galactic Power amounts above {{ format(DC.NUMMAX, 2, 2) }}.</span>
+    </div>
+    <div>
+      <span class="c-normal-ethereal-text">Your Cosmic Sector is </span>
+      <span class="c-really-cool-ethereal-text">{{ formatInt(cosmicSector) }}</span>
+      <span class="c-normal-ethereal-text">, which is currently multiplying all Celestial Dimensions and delaying
+      the Celestial Matter Softcap by </span>
+      <span class="c-really-cool-ethereal-text">{{ format(sectorBoost, 2, 2) }}</span><span class="c-normal-ethereal-text">.</span>
+    </div>
+    <div>
+      <span class="c-normal-ethereal-text">You will ascend into the next Cosmic Sector at </span>
+      <span class="c-really-cool-ethereal-text">{{ format(nextSectorAt, 2, 2) }}</span>
+      <span class="c-normal-ethereal-text"> Ethereal Power.</span>
     </div>
   </div>
 </template>
 
 <style scoped>
-.nyi-ethereal-text {
+.c-normal-ethereal-text {
   font-size: 2rem;
   color: #0000c0;
 }
 
-.really-cool-ethereal-text {
+.c-really-cool-ethereal-text {
   font-size: 3rem;
   font-weight: bold;
   background: linear-gradient(#000000, #0000c0);
