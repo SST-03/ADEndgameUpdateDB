579a4ad03224a11a8bc5ee3ff6c70dcee4251ac3
Supersonic Seven <b.rockholz@icloud.com>
2026-01-22 16:37:55 -0500

    Update dilation.js
    Power down

--- src/core/dilation.js
+++ src/core/dilation.js
@@ -130,7 +130,7 @@ export function getTachyonGalaxyMult(thresholdUpgrade) {
   return (1 + thresholdMult * glyphReduction) ** power;
 }
 
-export function getDilationGainPerSecond(diff) {
+export function getDilationGainPerSecond() {
   if (Pelle.isDoomed) {
     let pelleExtraDT = new Decimal(1);
     if (PelleAchievementUpgrade.achievement132.isBought) pelleExtraDT = pelleExtraDT.timesEffectsOf(Achievement(132));
@@ -149,7 +149,7 @@ export function getDilationGainPerSecond(diff) {
       .timesEffectsOf(DilationUpgrade.dtGain, DilationUpgrade.dtGainPelle, DilationUpgrade.flatDilationMult)
       .times(ShopPurchase.dilatedTimePurchases.currentMult ** 0.5)
       .times(Pelle.specialGlyphEffect.dilation).times(pelleExtraDT)
-      .times(diff).div(1000);
+      .times(getGameSpeedupForDisplay());
     if (dtRate.gte(DilationSoftcapStart.PRIMARY_THRESHOLD)) {
       dtRate = Decimal.pow(10, (((Decimal.log10(dtRate).sub(Decimal.log10(DilationSoftcapStart.PRIMARY_THRESHOLD))).div(10)).add(
         Decimal.log10(DilationSoftcapStart.PRIMARY_THRESHOLD))));
@@ -172,7 +172,7 @@ export function getDilationGainPerSecond(diff) {
     Decimal.clampMin(Decimal.log10(Replicanti.amount.add(1)).times(getAdjustedGlyphEffect("replicationdtgain")), 1));
   if (Enslaved.isRunning && !dtRate.eq(0)) dtRate = Decimal.pow10(Decimal.pow(dtRate.plus(1).log10(), 0.85).sub(1));
   if (V.isRunning) dtRate = dtRate.pow(0.5);
-  dtRate = dtRate.times(diff).div(1000);
+  dtRate = dtRate.times(getGameSpeedupForDisplay());
   if (dtRate.gte(DilationSoftcapStart.PRIMARY_THRESHOLD)) {
     dtRate = Decimal.pow(10, (((Decimal.log10(dtRate).sub(Decimal.log10(DilationSoftcapStart.PRIMARY_THRESHOLD))).div(10)).add(
       Decimal.log10(DilationSoftcapStart.PRIMARY_THRESHOLD))));
