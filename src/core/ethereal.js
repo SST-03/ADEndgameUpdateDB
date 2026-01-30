738d791d528b9fc51f82ecbb229d03a10aef9b24
Supersonic Seven <b.rockholz@icloud.com>
2026-01-30 15:38:08 -0500

    Update ethereal.js
    Fine-tune

--- src/core/ethereal.js
+++ src/core/ethereal.js
@@ -74,7 +74,7 @@ export function resetForStar(id) {
   const starName = gainedStarType.name;
   const resetReq = gainedStarType.resetReq;
   if (Currency.etherealPower.lt(resetReq)) return;
-  const resetFormula = Decimal.pow(Currency.etherealPower.div(resetReq), 0.5 - id / 20);
+  const resetFormula = Decimal.pow(Currency.etherealPower.value.div(resetReq), 0.5 - id / 20);
   player.endgame.ethereal.power = DC.D0;
   player.endgame.ethereal.star[starName] = player.endgame.ethereal.star[starName].add(resetFormula);
 }
