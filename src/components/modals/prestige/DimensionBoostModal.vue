a31d67ef2f6428e9ef7e4f9c1774132c871cb909
Supersonic Seven <b.rockholz@icloud.com>
2026-01-31 10:46:19 -0500

    Update DimensionBoostModal.vue
    Reader

--- src/components/modals/prestige/DimensionBoostModal.vue
+++ src/components/modals/prestige/DimensionBoostModal.vue
@@ -17,8 +17,8 @@ export default {
       return `You are about to do a Dimension Boost Reset`;
     },
     message() {
-      const keepDimensions = (Perk.antimatterNoReset.canBeApplied && !player.disablePostReality) || Achievement(111).canBeApplied ||
-        PelleUpgrade.dimBoostResetsNothing.isBought || PelleAchievementUpgrade.achievement111.isBought
+      const keepDimensions = (Perk.antimatterNoReset.canBeApplied || Achievement(111).canBeApplied ||
+        PelleUpgrade.dimBoostResetsNothing.isBought || PelleAchievementUpgrade.achievement111.isBought) && !player.disablePostReality
         ? `not actually reset anything due to an upgrade you have which prevents Antimatter and Antimatter Dimensions
           from being reset in this situation. You will still gain the multiplier from the Boost, as usual.`
         : `reset your Antimatter and Antimatter Dimensions. Are you sure you want to do this?`;
