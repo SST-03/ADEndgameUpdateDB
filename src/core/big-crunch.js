adba671520667607f1e5feb593d70df0ffe8714d
Supersonic Seven <b.rockholz@icloud.com>
2026-02-05 14:58:55 -0500

    Update big-crunch.js
    Final fix

--- src/core/big-crunch.js
+++ src/core/big-crunch.js
@@ -55,7 +55,7 @@ export function bigCrunchReset(
     EventHub.dispatch(GAME_EVENT.BIG_CRUNCH_BEFORE);
     bigCrunchGiveRewards();
     if (Pelle.isDoomed) PelleStrikes.infinity.trigger();
-    if (Alpha.isRunning && player.infinities.eq(1)) Alpha.advanceLayer();
+    if (Alpha.isRunning && player.infinities.eq(1) && Alpha.currentStage === 3) Alpha.advanceLayer();
   }
 
   bigCrunchResetValues(enteringAntimatterChallenge);
